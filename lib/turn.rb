05d2c74677406d5b741f5d9e07e56eaa7c7e05b2
Andrew Persad <akpersad@gmail.com>
2020-02-05 16:15:05 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,54 @@
+require "pry"
+def position_taken?(board, index)
+  if board[index] !~ /[^[:space:]]/
+    return false;
+  end
+
+  return true
+end
+
+def valid_move?(board, index)
+  binding.pry
+  if position_taken?(board, index) || !index.between?(0,8)
+    return false
+  end
+  return true
+end
+
+def input_to_index(arg)
+    if !!(arg =~ /\A[-+]?[0-9]+\z/) || arg == 0
+        arg = arg.to_i
+        if arg > 0
+            arg = arg - 1
+        else
+            arg
+        end
+    else
+        arg = -1
+    end
+end
+
+
+def move (board, position, choice = "X")
+  board[position] = choice
+  return board
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  input = input_to_index(input)
+  if !position_taken?(board, input)
+    move(board, input)
+  else
+    # error
+  end
+end
